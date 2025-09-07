a5be620d89152238021870ee8752effceba85632
tgilvonas <tgilvonas@gmail.com>
2025-09-07 18:45:54 +0300

    Translations

--- config/app.php
+++ config/app.php
@@ -78,11 +78,15 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
-
+    'locale' => env('APP_LOCALE', 'lt'),
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+
+    'admin_locales' => ['lt', 'en'],
+    'website_locales' => ['lt', 'en'],
+
+    'admin_locale' => env('APP_LOCALE', 'lt'),
 
     /*
     |--------------------------------------------------------------------------
