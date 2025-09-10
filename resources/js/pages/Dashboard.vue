7b72caee192f00bbc84caaa7d6b4e5f01dcab3f7
tgilvonas <tgilvonas@gmail.com>
2025-09-10 21:43:58 +0300

    Translations

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -4,6 +4,7 @@ import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import { trans } from '@/helpers/translator'
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -14,7 +15,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head :title="trans('dashboard')" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
