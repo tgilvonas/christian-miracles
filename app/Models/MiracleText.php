411a50d85d1366d78ee6d273a897476e2599b3b5
tgilvonas <tgilvonas@gmail.com>
2026-08-20 21:06:30 +0300

    Soft delete for MiracleText model

--- app/Models/MiracleText.php
+++ app/Models/MiracleText.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class MiracleText extends Model implements HasMedia
 {
-    use InteractsWithMedia;
+    use InteractsWithMedia, SoftDeletes;
 
     protected $guarded = ['id'];
 
