9daf029c625393adcb770663acc4a6275d5004fa
tgilvonas <tgilvonas@gmail.com>
2026-08-23 15:48:36 +0300

    Homepage was changed

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,43 +1,19 @@
 <script setup lang="ts">
-import { dashboard, login, register } from '@/routes';
 import { Head, Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Intro">
-        <link rel="preconnect" href="https://rsms.me/" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Head>
-    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-        <header class="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
-            <nav class="flex items-center justify-end gap-4">
-                <Link
-                    v-if="$page.props.auth.user"
-                    :href="dashboard()"
-                    class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                >
-                    Dashboard
-                </Link>
+    <div class="flex min-h-screen flex-col bg-[#dddddd] p-6 dark:text-[#ffffff] lg:p-8 dark:bg-[#000000]">
+        <header class="mb-6 w-full max-w-[1800px] text-sm not-has-[nav]:hidden">
+            <nav class="flex items-center justify gap-4">
+                <img src="/images/logo-lt.png" alt="Logo" class="max-h-[5rem]" />
             </nav>
         </header>
-        <div class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-            <main class="flex w-full max-w-[335px] flex-col-reverse overflow-hidden rounded-lg lg:max-w-4xl lg:flex-row">
-                <div
-                    class="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"
-                >
-                    <h1 class="mb-1 text-2xl mb-2">Krikščionių šventųjų ir jų stebuklų svetainė</h1>
-                    <p class="text-lg">Vyksta kūrimo darbai</p>
-                </div>
-                <div
-                    class="relative -mb-px aspect-335/376 w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]"
-                >
-                    <img src="/images/st-patrick-for-intro-screen.jpg" alt="St.Patrick" />
-                    <div
-                        class="absolute inset-0 rounded-t-lg shadow-[inset_0px_0px_0px_1px_rgba(0,25,25,0.16)] lg:overflow-hidden lg:rounded-t-none lg:rounded-r-lg dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"
-                    />
-                </div>
+        <div class="flex w-full items-center justify-center">
+            <main class="w-full max-w-[1800px]">
+                <h1 class="mb-1 text-2xl mb-2">Krikščionių šventųjų ir jų stebuklų svetainė</h1>
+                <p class="text-lg">Vyksta kūrimo darbai</p>
             </main>
         </div>
-        <div class="hidden h-14.5 lg:block"></div>
     </div>
 </template>
