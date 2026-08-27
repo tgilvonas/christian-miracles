6eccaad11baf3a39249658cd1cd7f27311809151
tgilvonas <tgilvonas@gmail.com>
2026-08-27 22:07:49 +0300

    Alignment of elements

--- resources/js/pages/Miracles.vue
+++ resources/js/pages/Miracles.vue
@@ -18,13 +18,13 @@ onMounted(async () => {
 
 <template>
     <div class="flex min-h-screen flex-col bg-[#dddddd] p-6 dark:text-[#ffffff] lg:p-8 dark:bg-[#000000]">
-        <header class="mb-6 w-full max-w-[1800px] text-sm not-has-[nav]:hidden">
+        <header class="mb-6 w-full max-w-[1800px] mx-auto text-sm not-has-[nav]:hidden">
             <nav class="flex items-center justify gap-4">
-                <img src="/images/logo-lt.png" alt="Logo" class="max-h-[5rem]" />
+                <img src="/images/logo-lt.png" alt="Logo" class="max-h-[6rem]" />
             </nav>
         </header>
         <div class="flex w-full items-center justify-center">
-            <main class="w-full max-w-[1800px]">
+            <main class="w-full max-w-[1800px] mx-auto">
                 <section class="grid gap-2">
                     <div v-if="!miracles.length">Loading...</div>
                     <article v-for="m in miracles" :key="m.id" class="flex items-center gap-4 rounded bg-white p-4 shadow dark:bg-[#111111]">
